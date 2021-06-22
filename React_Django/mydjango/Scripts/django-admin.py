26f7e5ef808cfa402234daebe2c415e5116b27ff
uni2237 <a01022371341@gmail.com>
2021-06-22 17:32:04 +0900

    django 프로젝트 생성, mysql db 연동

---
+++ React_Django/mydjango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a0102\onedrive - 충남대학교\바탕 화면\git\til\react_django\mydjango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
