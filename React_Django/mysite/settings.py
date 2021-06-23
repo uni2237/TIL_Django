ea96c6ebfa69ea2006b59248eb817fe1a4b47ce4
uni2237 <a01022371341@gmail.com>
2021-06-23 09:21:14 +0900

    settings.py 수정

--- React_Django/mysite/settings.py
+++ React_Django/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import React_Django.my_settings as my_settings
+import my_settings as my_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
